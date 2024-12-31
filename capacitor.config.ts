5be859a93ef39d31a1096182f324059cf39d1029
faienz93 <afaienza93@gmail.com>
2024-12-31 17:18:27 +0100

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'car.maintenance.app',
+  appName: 'car-maintenance-app',
+  webDir: 'www'
+};
+
+export default config;
