214193b5830865c8232448729916a277e7955270
faienz93 <afaienza93@gmail.com>
2025-01-04 15:56:37 +0100

    added back button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,39 +1,50 @@
 import React from 'react';
-import { IonContent, IonItem, IonLabel, IonList } from '@ionic/react';
+import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonToolbar, IonTitle, IonButtons, IonBackButton } from '@ionic/react';
 
 // import './homepage.css';
-import manutenzioneData from '../assets/data.json'; 
+import manutenzioneData from '../assets/data.json';
 
 
 function HomePage() {
 
-  const getRowClass = (tipo: string) => {
-    switch (tipo) {
-      case 'Tagliando':
-        return 'tagliando';
-      case 'Gomme':
-        return 'gomme';
-      case 'Revisione':
-        return 'revisione';
-      default:
-        return '';
-    }
-  };
+  // const getRowClass = (tipo: string) => {
+  //   switch (tipo) {
+  //     case 'Tagliando':
+  //       return 'tagliando';
+  //     case 'Gomme':
+  //       return 'gomme';
+  //     case 'Revisione':
+  //       return 'revisione';
+  //     default:
+  //       return '';
+  //   }
+  // };
 
   return (
-    <IonContent color="light">
-      <IonList inset={true}>
-        {manutenzioneData.map((item, index) => (
-          <IonItem>
-            <IonLabel>{item.data}</IonLabel>
-            <IonLabel>{item.km}</IonLabel>
-            <IonLabel>{item.tipo}</IonLabel>
-            <IonLabel>{item.costo}</IonLabel>
-            <IonLabel>{item.note || '—'}</IonLabel>
-          </IonItem>
-        ))}
-      </IonList>
-    </IonContent>
+    <>
+      <IonHeader translucent={true}>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/home"></IonBackButton>
+          </IonButtons>
+          <IonTitle>List Maintenance</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent color="light" fullscreen={true}>
+        <IonList inset={true}>
+          {manutenzioneData.map((item, index) => (
+            <IonItem>
+              <IonLabel>{item.data}</IonLabel>
+              <IonLabel>{item.km}</IonLabel>
+              <IonLabel>{item.tipo}</IonLabel>
+              <IonLabel>{item.costo}</IonLabel>
+              <IonLabel>{item.note || '—'}</IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
+      </IonContent>
+    </>
+
   );
 
 }
--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonToolbar, IonTitle, IonButtons, IonBackButton } from '@ionic/react';
 
 const ListPage = () => (
-  <>
-    <IonHeader>
-      <IonToolbar>
-        <IonTitle>Radio</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100%',
-        }}
-      >
-        List content
-      </div>
-    </IonContent>
-  </>
+    <>
+        <IonHeader>
+            <IonToolbar>
+                <IonButtons slot="start">
+                    <IonBackButton defaultHref="/home"></IonBackButton>
+                </IonButtons>
+                <IonTitle>List Maintenance</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        <IonContent>
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '100%',
+                }}
+            >
+                List content
+            </div>
+        </IonContent>
+    </>
 );
 
 export default ListPage;
