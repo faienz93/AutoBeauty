13e50196e81ac0ee072cb3287f214fc3e3db7ce1
faienz93 <afaienza93@gmail.com>
2025-01-04 13:10:17 +0100

    refactor code and swith on List

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/react';
-import './homepage.css';
+// import './homepage.css';
 const manutenzioneData = [
   { data: '21/08/19', km: 62000, tipo: 'Tagliando', costo: '€ 50,00', note: 'Cambio gomme + convergenza' },
   { data: '09/04/21', km: 74000, tipo: 'Gomme', costo: '€ 420,00', note: '' },
@@ -8,6 +8,7 @@ const manutenzioneData = [
   { data: '10/08/21', km: 81000, tipo: 'Tagliando', costo: '€ 50,00', note: '' },
   { data: '06/01/22', km: 83938, tipo: 'Gomme', costo: '€ 30,00', note: 'Inversione gomme' }
 ];
+import { IonContent, IonItem, IonLabel, IonList } from '@ionic/react';
 
 function HomePage() {
 
@@ -25,32 +26,19 @@ function HomePage() {
   };
 
   return (
-    <IonCard>
-      <IonCardHeader>
-        <IonCardTitle>Storico Manutenzione Auto</IonCardTitle>
-      </IonCardHeader>
-      <IonCardContent>
-        <IonGrid>
-          <IonRow className="header-row">
-            <IonCol size="2"><strong>Data</strong></IonCol>
-            <IonCol size="2"><strong>Al KM</strong></IonCol>
-            <IonCol size="2"><strong>Tipo</strong></IonCol>
-            <IonCol size="2"><strong>Costo (€)</strong></IonCol>
-            <IonCol size="4"><strong>Note</strong></IonCol>
-          </IonRow>
-
-          {manutenzioneData.map((item, index) => (
-            <IonRow key={index} className={getRowClass(item.tipo)}>
-              <IonCol size="2">{item.data}</IonCol>
-              <IonCol size="2">{item.km}</IonCol>
-              <IonCol size="2">{item.tipo}</IonCol>
-              <IonCol size="2">{item.costo}</IonCol>
-              <IonCol size="4">{item.note || '—'}</IonCol>
-            </IonRow>
-          ))}
-        </IonGrid>
-      </IonCardContent>
-    </IonCard>
+    <IonContent color="light">
+      <IonList inset={true}>
+        {manutenzioneData.map((item, index) => (
+          <IonItem>
+            <IonLabel>{item.data}</IonLabel>
+            <IonLabel>{item.km}</IonLabel>
+            <IonLabel>{item.tipo}</IonLabel>
+            <IonLabel>{item.costo}</IonLabel>
+            <IonLabel>{item.note || '—'}</IonLabel>
+          </IonItem>
+        ))}
+      </IonList>
+    </IonContent>
   );
 
 }
