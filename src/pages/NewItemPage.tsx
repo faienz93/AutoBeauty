52b63bb6676450967f10806512af0954c95f74d5
faienz93 <afaienza93@gmail.com>
2025-01-04 17:22:37 +0100

    refactor NewItem

--- src/pages/NewItemPage.tsx
+++ src/pages/NewItemPage.tsx
@@ -11,91 +11,67 @@ import {
   IonLabel,
   IonInput,
   IonSelect,
-  IonSelectOption
+  IonSelectOption,
+  IonAccordionGroup,
+  IonAccordion,
+  IonDatetimeButton,
+  IonDatetime
 } from '@ionic/react';
 
 
-interface Maintenance {
-  date: string;
-  km: number;
-  type: 'Tagliando' | 'Gomme' | 'Revisione';
-  cost: number;
-  note: string;
-}
+
+
 
 function NewItemPage() {
   console.log('Rendering NewItem component');
-  const [maintenances, setMaintenances] = useState<Maintenance[]>([]);
-  const [form, setForm] = useState<Partial<Maintenance>>({});
 
-  const handleAddMaintenance = () => {
-    if (form.date && form.km && form.type && form.cost) {
-      setMaintenances([...maintenances, form as Maintenance]);
-      setForm({});
-    }
-  };
 
-  const nextTagliando = () => {
-    const lastTagliando = maintenances.filter(m => m.type === 'Tagliando').pop();
-    if (lastTagliando) {
-      return lastTagliando.km + 15000;
-    }
-    return 15000;
-  };
+
+
+
+  // const nextTagliando = () => {
+  //   const lastTagliando = maintenances.filter(m => m.type === 'Tagliando').pop();
+  //   if (lastTagliando) {
+  //     return lastTagliando.km + 15000;
+  //   }
+  //   return 15000;
+  // };
 
   return (
-    <> 
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Maintenance</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <IonList>
-          {maintenances.map((m, index) => (
-            <IonItem key={index}>
-              <IonLabel>{`${m.date} - ${m.type} - ${m.km} km - €${m.cost}`}</IonLabel>
+    <>
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Maintenance</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen class="component-content">
+          <IonList inset={true}>
+            <IonItem lines="inset" slot="header">
+              <IonLabel position="floating">Data</IonLabel>
+              <IonInput type="date"></IonInput>
             </IonItem>
-          ))}
-        </IonList>
-
-        <IonItem>
-          <IonLabel position="floating">Data</IonLabel>
-          <IonInput type="date" onIonChange={e => setForm({ ...form, date: e.detail.value! })}></IonInput>
-        </IonItem>
-        <IonItem>
-          <IonLabel position="floating">Km</IonLabel>
-          <IonInput type="number" onIonChange={e => setForm({ ...form, km: parseInt(e.detail.value!, 10) })}></IonInput>
-        </IonItem>
-        <IonItem>
-          <IonLabel position="floating">Tipo</IonLabel>
-          {/* <IonSelect onIonChange={e => setForm({ ...form, type: e.detail.value })}>
-            <IonSelectOption value="Tagliando">Tagliando</IonSelectOption>
-            <IonSelectOption value="Gomme">Gomme</IonSelectOption>
-            <IonSelectOption value="Revisione">Revisione</IonSelectOption>
-          </IonSelect> */}
-         
+            <IonItem lines="inset" slot="header">
+              <IonLabel position="floating">Km</IonLabel>
+              <IonInput type="number" min={0}></IonInput>
+            </IonItem>
+            <IonItem lines="inset" slot="header">
+              <IonLabel position="floating">Tipo</IonLabel>
               <IonSelect aria-label="Maintenance" interface="action-sheet" placeholder="Select Maintenance">
                 <IonSelectOption value="Tagliando">Tagliando</IonSelectOption>
                 <IonSelectOption value="Gomme">Gomme</IonSelectOption>
                 <IonSelectOption value="Revisione">Revisione</IonSelectOption>
               </IonSelect>
-            
-        </IonItem>
-        <IonItem>
-          <IonLabel position="floating">Costo (€)</IonLabel>
-          <IonInput type="number" onIonChange={e => setForm({ ...form, cost: parseFloat(e.detail.value!) })}></IonInput>
-        </IonItem>
+            </IonItem>
+            <IonItem>
+              <IonLabel position="floating">Costo (€)</IonLabel>
+              <IonInput type="number" min={0}></IonInput>
+            </IonItem>
+            <IonButton expand="full">Aggiungi Manutenzione</IonButton>
+          </IonList>
+        </IonContent>
+      </IonPage>
 
-        <IonButton expand="full" onClick={handleAddMaintenance}>Aggiungi Manutenzione</IonButton>
-
-        <IonItem>
-          <IonLabel>Prossimo Tagliando: {nextTagliando()} km</IonLabel>
-        </IonItem>
-      </IonContent>
-    </IonPage>
-    
     </>
   );
 };
