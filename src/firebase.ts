32cca7389e17c117c18beb8568dbdda4863a4162
faienz93 <afaienza93@gmail.com>
2025-01-26 11:19:25 +0100

    created environment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,24 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getEnv } from './services/env';
+
+const envVar = getEnv("TEST");
+
+if (!envVar) {
+  throw new Error("Environment variables are not set correctly.");
+}
+
+
 
 const firebaseConfig = {
   // Copia qui le tue credenziali dalla Firebase Console
   // Project Settings -> General -> Your apps -> Web app (</>)
-  apiKey: "xxx",
-  authDomain: "xxx",
-  projectId: "xxx",
-  storageBucket: "xxx",
-  messagingSenderId: "xxx",
-  appId: "xxx"
+    apiKey: envVar?.apiKey,
+    authDomain: envVar?.authDomain,
+    projectId: envVar?.projectId,
+    storageBucket: envVar?.storageBucket,
+    messagingSenderId: envVar?.messagingSenderId,
+    appId: envVar?.appId,
 };
 
 const app = initializeApp(firebaseConfig);
---
+++ src/services/env/index.ts
@@ -0,0 +1,17 @@
+export const getEnv = (id: string) => {
+    const envName = `VITE_${id}_ENV_NAME`;
+    if (!import.meta.env[envName]) {
+        console.error(`Environment variable ${envName} is not defined`);
+        return;
+    }
+    
+    return {
+        apiKey: import.meta.env[`VITE_${id}_API_KEY`],
+        authDomain: import.meta.env[`VITE_${id}_AUTH_DOMAIN`],
+        projectId: import.meta.env[`VITE_${id}_PROJECT_ID`],
+        storageBucket: import.meta.env[`VITE_${id}_STORAGE_BUCKET`],
+        messagingSenderId: import.meta.env[`VITE_${id}_MESSAGING_SENDER_ID`],
+        appId: import.meta.env[`VITE_${id}_APP_ID`],
+        collection: import.meta.env[`VITE_${id}_COLLECTION`],
+    };
+};
