952fd9cb642256db317c775963f4167a4f94407f
faienz93 <afaienza93@gmail.com>
2025-05-25 16:50:02 +0200

    optimaze AlertConfirmation

--- src/components/AlertConfirmation.tsx
+++ src/components/AlertConfirmation.tsx
@@ -1,32 +1,35 @@
 import { IonAlert } from "@ionic/react";
+import { memo } from "react";
 
 
-export const AlertConfirmation = ({ msg, trigger, isOpen, onClose, onConfirm }: { msg: string, trigger: string, isOpen: boolean, onClose: () => void, onConfirm: () => void }) => {
-    console.log("Confirmation")
-    return (
-      <IonAlert
-        isOpen={isOpen} 
-        header="Attenzione"
-        message= {msg}
-        trigger={trigger}
-        buttons={[
-          {
-            text: 'Non sono sicuro',
-            role: 'cancel',
-            handler: () => {
-              console.log('Alert canceled');
-            },
-          },
-          {
-            text: 'OK',
-            role: 'confirm',
-            handler: () => {
-              console.log('Alert confirmed');
-              onConfirm()
-              onClose();
-            },
-          },
-        ]}
-        onDidDismiss={() => onClose()}></IonAlert>
-    );
-  };
+export const AlertConfirmation = memo(({ msg, trigger, isOpen, onClose, onConfirm }: { msg: string, trigger: string, isOpen: boolean, onClose: () => void, onConfirm: () => void }) => {
+  console.log("Confirmation")
+
+  const buttons = [
+    {
+      text: 'Non sono sicuro',
+      role: 'cancel',
+      handler: () => {
+        console.log('Alert canceled');
+      },
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+        onConfirm()
+        onClose();
+      },
+    },
+  ]
+  return (
+    <IonAlert
+      isOpen={isOpen}
+      header="Attenzione"
+      message={msg}
+      trigger={trigger}
+      buttons={buttons}
+      onDidDismiss={() => onClose()}></IonAlert>
+  );
+});
