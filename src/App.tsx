7c56a061042d91a099c89723675bb9bfdbc3e7ee
faienz93 <afaienza93@gmail.com>
2025-08-14 09:22:17 +0200

    feat: update route rendering method in App component

--- src/App.tsx
+++ src/App.tsx
@@ -63,13 +63,13 @@ function App() {
           <IonTabs>
             <IonRouterOutlet>
               <Redirect exact path="/" to="/home" />
-              <Route path="/home" render={() => <HomePage />} exact={true} />
-              <Route path="/newItem" render={() => <NewMaintenance />} exact={true} />
-              <Route path="/newItem/edit/:id" render={() => <UpdateMaitenance />} exact={true} />
-              <Route path="/list" render={() => <ListCarMaintenance />} exact={true} />
-              <Route path="/newkm/edit/:id" render={() => <KmPage />} exact={true} />
-              <Route path="/settings" render={() => <Setting />} exact={true} />
-              <Route path="/info" render={() => <InfoPage />} exact={true} />
+              <Route path="/home" component={HomePage} exact={true} />
+              <Route path="/newItem" component={NewMaintenance} exact={true} />
+              <Route path="/newItem/edit/:id" component={UpdateMaitenance} exact={true} />
+              <Route path="/list" component={ListCarMaintenance} exact={true} />
+              <Route path="/newkm/edit/:id" render={KmPage} exact={true} />
+              <Route path="/settings" component={Setting} exact={true} />
+              <Route path="/info" component={InfoPage} exact={true} />
             </IonRouterOutlet>
 
             <IonTabBar slot="bottom">
